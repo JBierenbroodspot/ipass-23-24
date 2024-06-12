4c9784224ea9ad78083bdf5baf462e7b27f3d2ae
JBierenbroodspot <jim12.brood@gmail.com>
2024-06-12 15:19:39 +0200

    Add TwoDType

--- src/test/test_types.py
+++ src/test/test_types.py
@@ -4,12 +4,13 @@ import numpy as np
 import numpy.testing as nptest
 import numpy.typing as npt
 
-from src.types import OneDArray
+from src.types import OneDArray, TwoDArray
 from src.exceptions import DimensionError
 
 
 class TestOneDArray(unittest.TestCase):
-    def test_successful_use(self) -> None:
+    @staticmethod
+    def test_successful_use() -> None:
         one_d_array: npt.NDArray = np.array([1, 2, 3, 4])
 
         result: OneDArray = OneDArray(one_d_array)
@@ -21,3 +22,25 @@ class TestOneDArray(unittest.TestCase):
 
         with self.assertRaises(DimensionError):
             OneDArray(two_d_array)
+
+
+class TestTwoDArray(unittest.TestCase):
+    def test_one_d_array(self) -> None:
+        one_d_array: npt.NDArray = np.array([1, 2, 3, 4])
+
+        with self.assertRaises(DimensionError):
+            TwoDArray(one_d_array)
+
+    @staticmethod
+    def test_two_d_array() -> None:
+        two_d_array: npt.NDArray = np.array([[1, 2], [1, 2]])
+
+        result: TwoDArray = TwoDArray(two_d_array)
+
+        nptest.assert_array_equal(two_d_array, result())
+
+    def test_three_d_array(self) -> None:
+        three_d_array: npt.NDArray = np.zeros(shape=(3, 3, 3))
+
+        with self.assertRaises(DimensionError):
+            TwoDArray(three_d_array)
--- src/types.py
+++ src/types.py
@@ -17,3 +17,21 @@ class OneDArray[DTypeT: npt.DTypeLike]:
 
     def __call__(self) -> npt.NDArray[DTypeT]:
         return self._arr
+
+
+class TwoDArray[DTypeT: npt.DTypeLike]:
+    _arr: npt.NDArray[DTypeT]
+
+    def __init__(self, arr: npt.NDArray[DTypeT]) -> None:
+        if len(arr.shape) == 1:
+            raise DimensionError(
+                "Trying to assign one-dimensional array to TwoDArray, use `OneDArray` instead."
+            )
+
+        if len(arr.shape) > 2:
+            raise DimensionError("Too many dimensions for TwoDArray.")
+
+        self._arr = arr
+
+    def __call__(self) -> npt.NDArray[DTypeT]:
+        return self._arr
