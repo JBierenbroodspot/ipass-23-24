aa5500a2628fe65d35a5ff4e5fdee3d875d7adbc
JBierenbroodspot <jim12.brood@gmail.com>
2024-06-13 14:07:34 +0200

    Delete use of custom datatypes

--- src/algorithm/clustering.py
+++ src/algorithm/clustering.py
@@ -1,29 +1,32 @@
-from typing import TypeVar, Union
+from typing import Any
 
 import numpy as np
 import numpy.typing as npt
 
-from src.types import OneDArray, TwoDArray
+# from src.types import OneDArray, TwoDArray
 
-DataT: TypeVar = TypeVar("DataT", *np.ScalarType)
+# DataT: TypeVar = TypeVar("DataT", *np.ScalarType)
 
 
-class ClusteringModel:
+class ClusteringModel[DataT: Any]:
     num_clusters: int
-    cluster_centers: TwoDArray[Union[DataT, np.float64]]
+    cluster_centers: npt.NDArray[np.float64]
 
-    def __init__(self, data: TwoDArray[DataT], num_clusters: int) -> None:
+    def __init__(self, data: npt.NDArray[DataT], num_clusters: int) -> None:
         self.num_clusters = num_clusters
         self.cluster_centers = self.get_random_cluster_centers(data, num_clusters)
 
     @staticmethod
     def get_random_cluster_centers(
-        data: TwoDArray[DataT], num_clusters: int
-    ) -> TwoDArray:
-        return TwoDArray(
-            data()[np.random.choice(data().shape[0], num_clusters, replace=False)]
-        )
+        data: npt.NDArray[DataT], num_clusters: int
+    ) -> npt.NDArray[DataT]:
+        return data[np.random.choice(data.shape[0], num_clusters, replace=False)]
 
     @staticmethod
-    def get_distance(x: OneDArray[DataT], y: OneDArray[DataT]) -> float:
-        return np.linalg.norm(x() - y())
+    def get_distance(
+        X: npt.NDArray[DataT], y: npt.NDArray[DataT]
+    ) -> npt.NDArray[np.float64]:
+        if len(X.shape) == 1:
+            return np.array(np.linalg.norm(X - y))
+
+        return np.array([np.linalg.norm(arr - y) for arr in X])
--- test/algorithm/test_clustering.py
+++ test/algorithm/test_clustering.py
@@ -1,53 +1,61 @@
 import unittest
-import operator
 
 import numpy as np
+import numpy.typing as npt
 import numpy.testing as nptest
 
 from src.algorithm.clustering import ClusteringModel
-from src.types import TwoDArray, OneDArray
 
 np.random.seed(1)
 
-TEST_TWO_D_ARR: TwoDArray[int] = TwoDArray(
-    np.array(
-        [
-            [1, 4, 6, 1],
-            [3, 5, 1, 4],
-            [1, 5, 0, 1],
-            [8, 1, 4, 6],
-            [9, 9, 9, 9],
-        ]
-    )
+TEST_ARR: npt.NDArray[np.int64] = np.array(
+    [
+        [1, 4, 6, 1],
+        [3, 5, 1, 4],
+        [1, 5, 0, 1],
+        [8, 1, 4, 6],
+        [9, 9, 9, 9],
+    ]
 )
 
 
 class TestClusteringModel(unittest.TestCase):
+    model: ClusteringModel[np.int64]
+    num_clusters: int
+
+    def setUp(self) -> None:
+        self.num_clusters = 2
+        self.model = ClusteringModel(TEST_ARR, self.num_clusters)
+
     def test_get_random_cluster_centers(self) -> None:
-        num_clusters: int = 2
-        result: np.ndarray = ClusteringModel.get_random_cluster_centers(
-            TEST_TWO_D_ARR, num_clusters
-        )()
+        result: npt.NDArray[np.int64] = self.model.get_random_cluster_centers(
+            TEST_ARR, self.num_clusters
+        )
 
         self.assertNotIsInstance(result[1, 1], int)
-        self.assertTrue(len(result) == num_clusters)
+        self.assertTrue(len(result) == self.num_clusters)
         with self.assertRaises(AssertionError):
             nptest.assert_array_equal(result[0], result[1])
 
     def test_init(self) -> None:
-        num_clusters: int = 2
-        model: ClusteringModel = ClusteringModel(TEST_TWO_D_ARR, num_clusters)
+        self.assertEqual(self.model.num_clusters, self.num_clusters)
+        self.assertEqual(len(self.model.cluster_centers), self.num_clusters)
 
-        self.assertEqual(model.num_clusters, num_clusters)
-        self.assertNotEqual(model.cluster_centers, None)
+        self.num_clusters = 3
+        self.model: ClusteringModel[np.int64] = ClusteringModel(
+            TEST_ARR, self.num_clusters
+        )
 
-    def test_get_distance(self) -> None:
+        self.assertEqual(self.model.num_clusters, self.num_clusters)
+        self.assertEqual(len(self.model.cluster_centers), self.num_clusters)
+
+    def test_get_distance_vector_to_vector(self) -> None:
         expected: float = np.sqrt(
             (1 - 3) ** 2 + (4 - 5) ** 2 + (6 - 1) ** 2 + (1 - 4) ** 2
         )
 
-        result: float = ClusteringModel.get_distance(
-            OneDArray(TEST_TWO_D_ARR()[0]), OneDArray(TEST_TWO_D_ARR()[1])
+        result: npt.NDArray[np.float64] = ClusteringModel[np.int64].get_distance(
+            TEST_ARR[0], TEST_ARR[1]
         )
 
         self.assertEqual(expected, result)
